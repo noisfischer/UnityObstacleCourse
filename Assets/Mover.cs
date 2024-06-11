ca4744dd482019825379d75cb82b5e29e6354508
noisfischer <noisfischer@gmail.com>
2024-06-11 12:06:05 -0700

    created MovePlayer function

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // SerializeField lets you edit variable values in Unity inspector panel
-    [SerializeField] float moveSpeed = 5.0f;
-
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
+    {
+        MovePlayer();
+    }
+
+    // SerializeField lets you edit variable values in Unity inspector panel
+    [SerializeField] float moveSpeed = 5.0f;
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0.0f, zValue);
     }
+
 }
